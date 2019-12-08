9c52a3c34996bb05de6335d28f583b27a262d1ca
George Travis <agtravis85@gmail.com>
2019-12-07 19:13:50 -0800

    JS creates button choices, no functionality after

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -15,7 +15,7 @@ document.getElementById('begin').addEventListener('click', function () {
 
 
 function start() {
-    var timeLeft = 5;
+    var timeLeft = 1;
 
     var timeInterval = setInterval(function () {
         timeToStart.textContent = 'This quiz will start in ' + (timeLeft - 1) + '...';
@@ -30,38 +30,66 @@ function start() {
     }, 1000);
 }
 
-var questionArr = ['question 1', 'question 2', 'question 3'];
-var answerArr = ['answer 1', 'answer 2', 'answer 3'];
 
 
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    ///etc.
+];
+//set score to zero
+var score = 0;
 function questionTime() {
-    //set score to zero
-    var score = 0;
+
+    //set question to zero
+    var question = 0;
     //create new element to hold question
     var newQuestion = document.createElement('p');
     //attach new empty question element to div
     mainContent.appendChild(newQuestion);
     //answer is blank
     var newAnswer = '';
-
-    for (var i = 0; i < 3; i++) {
-        //set new question to question drawn from array
-        newQuestion.textContent = questionArr[i];
-        //set new answer to correct answer drawn from array
-        newAnswer.textContent = answerArr[i];
-        //ask user for answer
-        var userAnswer = prompt('answer');
-        //compare answer to question
-        if (userAnswer === newAnswer) {
-            //if correct score one point
-            score++;
-        }
-        //if wrong 
-        // reset content to ''
-
+    //set new question to question drawn from array
+    newQuestion.textContent = questions[question].title;
+    //set new answer to correct answer drawn from array
+    newAnswer.textContent = questions[question].answer;
+    //ask user for answer
+    for (var i = 0; i < questions[question].choices.length; i++) {
+        var answerButton = document.createElement('button');
+        answerButton.setAttribute('class', 'answer');
+        answerButton.textContent = questions[question].choices[i];
+        mainContent.appendChild(answerButton);
     }
-    alert(score);
 
+
+
+
+    
+    // document.querySelector('.answer').addEventListener('click', function () {
+    //     alert(this.value);
+    //     if (this.innerText === questions[question].answer) {
+    //         console.log('score!');
+    //         score++;
+    //     }
+    // });
+    // var userAnswer = prompt(questions[question].choices);
+    //compare answer to question
+
+    //if wrong 
+    // reset content to ''
+
+
+
+    console.log(score);
+}
     // document.getElementById('answerButton').addEventListener('click', function () {
     //     alert('You clicked continue');
     //     if (question < 3) {
@@ -71,4 +99,3 @@ function questionTime() {
     //     }
     // });
 
-}
