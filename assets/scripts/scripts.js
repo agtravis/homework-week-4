0232fc2cb865bdd289e17e51a0af13a2bc3c0098
George Travis <agtravis85@gmail.com>
2019-12-07 20:59:39 -0800

    still only one loop works but with html buttons coded

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -5,6 +5,9 @@
 var mainContent = document.getElementById('main');
 var timeToStart = document.getElementById('countdown');
 
+var questionBox = document.getElementById('questionBox');
+var answerBox = document.getElementById('answerBox');
+
 document.getElementById('begin').addEventListener('click', function () {
     timeToStart.textContent = 'This quiz will start in 5...'
     start();
@@ -44,53 +47,55 @@ var questions = [
     },
     ///etc.
 ];
-//set score to zero
 var score = 0;
-//set question to zero
 var question = 0;
 function questionTime() {
-
-    //create new element to hold question
-    var newQuestion = document.createElement('p');
-    //attach new empty question element to container
-    mainContent.appendChild(newQuestion);
-    //create div to hold answer selection
-    var answerBox = document.createElement('div');
-    mainContent.appendChild(answerBox);
-    //answer is blank
-    var newAnswer = '';
+    var newAnswer;
     //set new question to question drawn from array
-    newQuestion.textContent = questions[question].title;
+    questionBox.textContent = questions[question].title;
     //set new answer to correct answer drawn from array
     newAnswer = questions[question].answer;
-    //ask user for answer
+    //create answer choices
     for (var i = 0; i < questions[question].choices.length; i++) {
-        var answerButton = document.createElement('button');
-        answerButton.setAttribute('id', 'answerButton' + i);
+        var answerButton = document.getElementById('btn' + i);
         answerButton.textContent = questions[question].choices[i];
-        answerBox.appendChild(answerButton);
     }
+    //take user choice and compare to actual answer
     answerBox.addEventListener('click', function () {
         if (event.target.nodeName === 'BUTTON') {
             userAnswer = event.target.textContent;
             if (userAnswer === newAnswer) {
                 alert('Correct!');
                 score++;
-                alert(score);
             } else {
                 alert('Wrong!');
-                alert(score);
             }
+            alert(score);
             question++;
-            while (mainContent.firstChild) {
-                mainContent.removeChild(mainContent.firstChild);
-            }
         }
     });
-
-
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (var i = 0; i < questions[question].choices.length; i++) {
+    //     var answerButton = document.createElement('button');
+    //     answerButton.setAttribute('id', 'answerButton' + i);
+    //     answerButton.textContent = questions[question].choices[i];
+    //     answerBox.appendChild(answerButton);
+    // }
+//elem.parentNode.removeChild(elem);
 // while (el.firstChild) el.removeChild(el.firstChild);
 // document.querySelector('.answer').addEventListener('click', function () {
 //     alert(this.value);
