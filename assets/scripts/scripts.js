d6cf0ebe081176992ce5a5e2788e3dc5baf3129f
George Travis <agtravis85@gmail.com>
2019-12-08 12:39:39 -0800

    only 2 loops working

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,7 +1,3 @@
-
-
-
-
 var mainContent = document.getElementById('main');
 var timeToStart = document.getElementById('countdown');
 
@@ -12,8 +8,6 @@ document.getElementById('begin').addEventListener('click', function () {
     timeToStart.textContent = 'This quiz will start in 5...'
     start();
 });
-
-
 
 function start() {
     //change this to 5
@@ -32,8 +26,6 @@ function start() {
     }, 1000);
 }
 
-
-
 var questions = [
     {
         title: "Commonly used data types DO NOT include:",
@@ -45,14 +37,24 @@ var questions = [
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
         answer: "parentheses"
     },
-    ///etc.
+    {
+        title: "Pick C",
+        choices: ["quotes", "curly brackets", "C", "square brackets"],
+        answer: "C"
+    },
+    {
+        title: "Pick D",
+        choices: ["quotes", "curly brackets", "parentheses", "D"],
+        answer: "D"
+    }
 ];
+
 var score = 0;
 var question = 0;
 var newAnswer;
 function questionTime() {
-    questionBox.textContent = nextQuestion(question);
-    newAnswer = nextAnswer(question);
+    questionBox.textContent = questions[question].title;
+    newAnswer = questions[question].answer;
     for (var i = 0; i < questions[question].choices.length; i++) {
         var answerButton = document.createElement('button');
         answerButton.setAttribute('id', 'answerButton' + i);
@@ -68,13 +70,12 @@ function questionTime() {
             } else {
                 alert('Wrong!');
             }
-            console.log(score);
-            questionBox.textContent = '';
-            while (answerBox.firstChild) {
-                answerBox.removeChild(answerBox.firstChild);
-            }
-            question++;
-            if (question < questions.length) {
+            if (question < questions.length - 1) {
+                question++;
+                questionBox.textContent = '';
+                while (answerBox.firstChild) {
+                    answerBox.removeChild(answerBox.firstChild);
+                }
                 questionTime();
             } else {
                 alert('finished! Your score is ' + score + ' out of ' + questions.length + '!');
@@ -83,13 +84,6 @@ function questionTime() {
     });
 }
 
-function nextQuestion(question) {
-    return questions[question].title;
-}
-
-function nextAnswer(question) {
-    return questions[question].answer;
-}
 
 
 
@@ -97,37 +91,5 @@ function nextAnswer(question) {
 
 
 
-// for (var i = 0; i < questions[question].choices.length; i++) {
-//     var answerButton = document.getElementById('btn' + i);
-//     answerButton.textContent = questions[question].choices[i];
-// }
 
-
-
-
-
-//elem.parentNode.removeChild(elem);
-// while (el.firstChild) el.removeChild(el.firstChild);
-// document.querySelector('.answer').addEventListener('click', function () {
-//     alert(this.value);
-//     if (this.innerText === questions[question].answer) {
-//         console.log('score!');
-//         score++;
-//     }
-// });
-// var userAnswer = prompt(questions[question].choices);
-//compare answer to question
-
-//if wrong 
-// reset content to ''
-
-
-    // document.getElementById('answerButton').addEventListener('click', function () {
-    //     alert('You clicked continue');
-    //     if (question < 3) {
-    //         questionTime();
-    //     } else {
-    //         alert('finished!');
-    //     }
-    // });
 
