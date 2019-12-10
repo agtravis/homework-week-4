680ef3c5ba46aececac1ffbf2f8c6ef9d1c2f71c
George Travis <agtravis85@gmail.com>
2019-12-09 18:39:32 -0800

    completely restarted, quiz works great. Need questions and timer for questions or quiz

--- assets/scripts/questions.js
+++ assets/scripts/questions.js
@@ -1,27 +1,47 @@
 var questions = [
-  {
-      title: "The answer is A:",
-      choices: ["A", "B", "C", "D"],
-      answer: "A"
-  },
-  {
-      title: "The answer is B:",
-      choices: ["A", "B", "C", "D"],
-      answer: "B"
-  },
-  {
-      title: "The answer is C:",
-      choices: ["A", "B", "C", "D"],
-      answer: "C"
-  },
-  {
-      title: "The answer is D:",
-      choices: ["A", "B", "C", "D"],
-      answer: "D"
-  },
-  {
-      title: "The answer is A:",
-      choices: ["A", "B", "C", "D"],
-      answer: "A"
-  }
+    {
+        title: "The answer is A:",
+        answers: [
+            { text: "A", correct: true },
+            { text: "B", correct: false },
+            { text: "C", correct: false },
+            { text: "D", correct: false }
+        ]
+    },
+    {
+        title: "The answer is F:",
+        answers: [
+            { text: "E", correct: false },
+            { text: "F", correct: true },
+            { text: "G", correct: false },
+            { text: "H", correct: false }
+        ]
+    },
+    {
+        title: "The answer is K:",
+        answers: [
+            { text: "I", correct: false },
+            { text: "J", correct: false },
+            { text: "K", correct: true },
+            { text: "L", correct: false }
+        ]
+    },
+    {
+        title: "The answer is P:",
+        answers: [
+            { text: "M", correct: false },
+            { text: "N", correct: false },
+            { text: "O", correct: false },
+            { text: "P", correct: true }
+        ]
+    },
+    {
+        title: "The answer is Q:",
+        answers: [
+            { text: "Q", correct: true },
+            { text: "R", correct: false },
+            { text: "S", correct: false },
+            { text: "T", correct: false }
+        ]
+    }
 ];
--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,11 +1,19 @@
-var mainContent = document.getElementById('main');
+//getting elements from the HTML and storing them as variables
 var timeToStart = document.getElementById('countdown');
+var startButton = document.getElementById('start-btn');
+var questionContainerElement = document.getElementById('question-container');
+var questionElement = document.getElementById('question');
+var answerButtonsElement = document.getElementById('answer-buttons');
+var finalScoreElement = document.getElementById('submit-score');
+var userScore = document.getElementById('score');
+var total = document.getElementById('total');
 
-var questionBox = document.getElementById('questionBox');
-var answerBox = document.getElementById('answerBox');
+//set variables that will increase, to start at zero
+var currentQuestionIndex = 0;
+var score = 0;
 
-document.getElementById('begin').addEventListener('click', function () {
-    timeToStart.textContent = 'This quiz will start in 5...'
+startButton.addEventListener('click', function () {
+    timeToStart.textContent = 'This quiz will start in 5...';
     start();
 });
 
@@ -20,52 +28,74 @@ function start() {
         if (timeLeft === 0) {
             clearInterval(timeInterval);
             timeToStart.textContent = '';
-            questionTime();
+            quizTime();
         }
 
     }, 1000);
 }
 
-var score = 0;
-var question = 0;
-var newAnswer = '';
-function questionTime() {
-    questionBox.textContent = questions[question].title;
-    newAnswer = questions[question].answer;
-    for (var i = 0; i < questions[question].choices.length; i++) {
-        var answerButton = document.createElement('button');
-        answerButton.textContent = questions[question].choices[i];
-        answerBox.appendChild(answerButton);
+function quizTime() {
+    //first hide the start button
+    startButton.classList.add('hide');
+    //get the hidden question container div and show it
+    questionContainerElement.classList.remove('hide');
+    //run the function to set the question
+    nextQuestion();
+}
+
+function nextQuestion() {
+    //as long as the answer buttons div has a first child, i.e. any contents
+    while (answerButtonsElement.firstChild) {
+        //remove that first child
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
     }
-    answerBox.addEventListener('click', function () {
-        if (event.target.nodeName === 'BUTTON') {
-            userAnswer = event.target.textContent;
+    //set the question to the property 'title' for the current iteration index of the questions array
+    questionElement.innerText = questions[currentQuestionIndex].title;
+    //get the array of answer objects currently iterator on, and run a forEach loop on it. 'answer' is the parameter name I have given to each item in the aray
+    questions[currentQuestionIndex].answers.forEach(function (answer) {
+        //create a button
+        var button = document.createElement('button');
+        //assign the 'text' property value of the array object to the inner text of the button
+        button.innerText = answer.text;
+        //add the 'btn' class to the button
+        button.classList.add('btn');
+        //if answer.correct has a value of true (since only one of the four answers is assigned true)
+        if (answer.correct) {
+            //create a data attribute for the button that has the value of 'answer.correct'. Note the false answers do not get assigned a data attribute
+            button.dataset.correct = answer.correct;
         }
-        if (userAnswer === newAnswer) {
-            alert('Correct!');
-            score++;
-        } else {
-            alert('Wrong!');
-        }
-        questionBox.textContent = '';
-        while (answerBox.firstChild) {
-            answerBox.removeChild(answerBox.firstChild);
-        }
-        if (question < questions.length - 1) {
-            question++;
-            questionTime();
-        } else {
-            alert('finished! Your score is ' + score + ' out of ' + questions.length + '!');
-        }
+        //add a listener for a click on a button, and if there is one pass the event to the function selectAnswer, which in turn passes the event
+        button.addEventListener('click', function (e) {
+            selectAnswer(e);
+        });
+        //append the button to the div
+        answerButtonsElement.appendChild(button);
     });
 }
 
-
-
-
-
-
-
-
-
-
+//handling the answer. The event type is passed
+function selectAnswer(e) {
+    //store the event (click) target (button) to a variable
+    var selectedButton = e.target;
+    // set a new variable to hold whether or not the data attribute exists - remember if it was false it does NOT have this attribute, and returns false
+    var correct = selectedButton.dataset.correct;
+    // if the answer is the correct one
+    if (correct) {
+        //the score increases by one
+        score++;
+    }
+    //if the index of the current question is less than the number of questions in the array (subtracting one to account for the last index being one less than the length)
+    if (currentQuestionIndex < questions.length - 1) {
+        //increase the question index
+        currentQuestionIndex++;
+        //run the next question function
+        nextQuestion();
+    } else {
+        //hide the game and show the finishing div with the dynamic data
+        questionContainerElement.classList.add('hide');
+        userScore.innerText = score;
+        total.innerText = questions.length;
+        finalScoreElement.classList.remove('hide');
+        //this will submit to storage
+    }
+}
